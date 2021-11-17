ac3787e10a0637c553124e4ffa5b75401f4a1a3d
Alexander Popov <iiiypuk@iiiypuk.me>
2021-11-18 01:13:18 +0300

    fix bug in script

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,7 +2,7 @@ function layoutReplace()
 {
   'use strict';
 
-  keys = {
+  let keys = {
     "q":"й", "w":"ц", "e":"у", "r":"к", "t":"е", "y":"н", "u":"г", 
     "i":"ш", "o":"щ", "p":"з", "[":"х", "]":"ъ", "a":"ф", "s":"ы", 
     "d":"в", "f":"а", "g":"п", "h":"р", "j":"о", "k":"л", "l":"д", 
@@ -13,9 +13,6 @@ function layoutReplace()
   let inputText = document.getElementById("one").value;
   let outputText = document.getElementById("two");
   let text = null;
-
-  console.log(inputText);
-
 
   text = inputText.replace(/[A-z/,.;\'\]\[]/g, function(x) {
     return x == x.toLowerCase() ? keys[ x ] : keys[ x.toLowerCase() ].toUpperCase();
